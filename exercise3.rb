f7ce69c040af0ce77354f62c18852477f912c2d5
Stephen H <stephen210huang@hotmail.com>
2017-01-16 15:04:34 -0500

    exercise3.rb completed

--- exercise3.rb
+++ exercise3.rb
@@ -6,3 +6,36 @@ puts "How old are you?"
 age = gets.to_i
 year = 2016 - age
 puts "You were born in #{year}."
+
+
+if 2 > 1
+  puts "I get printed!"
+end
+
+if 0 > 1
+  puts "I will not get printed!"
+end
+
+number = 10
+if number > 0
+  puts "#{number} is positive"
+else
+  puts "#{number} is negative"
+end
+
+
+counter = 1
+while counter < 5
+  puts "counter currently at #{counter}"
+  counter += 1
+end
+
+one_to_five = (1..5)
+one_to_five.each do |num|
+  puts num
+end
+
+one_to_five = (1..5)
+one_to_five.each do |num|
+  puts (num ** 2).to_s
+end
