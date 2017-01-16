9bfdd2f49b4733fe3b21e21d6774d4ad748eb08f
Stephen H <stephen210huang@hotmail.com>
2017-01-16 14:46:45 -0500

    exercise2.rb completed

--- exercise2.rb
+++ exercise2.rb
@@ -18,3 +18,11 @@ puts "The answer for 45628 * 7839 is #{45628 * 7839}."
 # What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)?
 puts "My guess is true."
 puts "The actual answer is #{(10 < 20 && 30 < 20) || !(10 == 11)}."
+
+
+amount = 20
+new_amount = amount
+new_amount # 20
+amount = "twenty"
+amount # "twenty"
+new_amount # 20
