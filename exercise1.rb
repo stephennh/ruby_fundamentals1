09d08611a24eba6dfc91b1f7e4262fc12b5d0daf
Stephen H <stephen210huang@hotmail.com>
2017-01-16 14:33:44 -0500

    exercise1.rb completed

--- exercise1.rb
+++ exercise1.rb
@@ -14,3 +14,7 @@ puts "Hello\t\tworld"
 puts "Hello\b\b\b\b\bGoodbye world"
 puts "Hello\rStart over world"
 puts "1. Hello\n2. World"
+
+puts (1 == 2) || (2 < 3)
+puts (1 == 2) || (3 < 2)
+puts (2 == 2) || (3 < 2)
