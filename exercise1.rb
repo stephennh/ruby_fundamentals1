13ebfd46a430f2d76f189e61e03e50281218e09a
Stephen H <stephen210huang@hotmail.com>
2017-01-16 14:28:57 -0500

    exercise1.rb escape sequences

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,16 @@
 puts 2
 puts 3
 puts 2 + 3
+
+puts "Hello World"
+puts 'Hello world'
+
+puts "Betty's World"
+puts 'Betty\'s World'
+
+puts "Ade Lovelace lived for #{1852 - 1815} years"
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
